3c5bec9248647d9090016869e2bf740bca163a08
Lisset Rico <lrico5@uic.edu>
2024-11-15 07:31:09 -0600

    environment variables updated for vercel

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,6 +1,19 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import firebaseConfig from '../../client-credentials.json' with { type: 'json' }
+// import firebaseConfig from '../../client-credentials.json' with { type: 'json' }
 
-export const app = initializeApp(firebaseConfig)
+// export const app = initializeApp(firebaseConfig)
+
+// Call Vercel Client environment variables
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  };
+  
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,12 +1,26 @@
 import type { ServiceAccount } from 'firebase-admin'
 import { initializeApp, cert } from 'firebase-admin/app'
-import serviceAccount from '../../server-credentials.json' with { type: 'json' }
+// import serviceAccount from '../../server-credentials.json' with { type: 'json' }
 
 if (import.meta.env.PUBLIC_EMULATOR === '1') {
     console.log('Emulator Setting Up')
     process.env['FIREBASE_AUTH_EMULATOR_HOST'] = '127.0.0.1:9099'
     process.env['FIRESTORE_EMULATOR_HOST'] = '127.0.0.1:8080'
 }
+
+// Call Vercel Server environment variables
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  };
 
 export const app = initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
